28f8ffae6d371257bf4c5bb8961e31b93725746c
lu607 <lunarojas2533@gmail.com>
2024-12-02 20:30:21 -0500

    Api- Luna

---
+++ Api2/src/modelo/paquete.js
@@ -0,0 +1,40 @@
+
+
+import mongoose from "mongoose";
+
+const PaqueteSchema = new mongoose.Schema({
+
+  destino: { 
+    type: String, 
+    required: true 
+  },
+  actividad: { 
+     type: String,
+     required: true
+     },
+  numeroPersonas: {
+     type: Number, 
+     required: true 
+    },
+  categoria: { 
+    type: String, 
+    enum: ['paquete1', 'paquete2', 'paquete3'],
+    required: true
+   },
+  precio: { 
+    type: Number,
+     required: true
+     },
+  transporte:
+   { type: String, 
+    required: true 
+  },
+  comida: 
+  { type: String,
+     required: true 
+    },
+},
+{ versionKey: false  }
+);
+
+export default mongoose.model("Paquete", PaqueteSchema);
